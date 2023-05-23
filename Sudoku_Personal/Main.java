84a1fecbc034dabcd6b545514aa62afa37171ef5
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-05-24 00:21:15 +0200

    Update Main.java
    Ampliando toda la interfaz para la entrada y modificación de la tabla. Pendiente de revisión.

--- Sudoku_Personal/Main.java
+++ Sudoku_Personal/Main.java
@@ -1,37 +1,81 @@
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner teclado = new Scanner(System.in);
-        int prueba = -1;
-        System.out.println("Por favor inserte (prueba):");
-        do{
-            try{
-                prueba = teclado.nextInt();
-            }
-            catch (InputMismatchException e) { //Muestra error en caso de no recibirse números
-                System.out.println("Valor incorrecto. Por favor introduce un numero.");
-                teclado.nextLine(); //teclado.nextLine() limpia el valor introducido en el teclado, evitando que el mensaje se muestre indefinidamente.
-            }
-        }  while (prueba < 0 || prueba > 9);
 
-        System.out.println("El número escogido es: " + prueba);
+        boolean correcto = true; //Booleano para determinar si se debe corregir algún valor
+        char respuesta = 'x';
+        int input = -1;
+        int [][] sudoku = {{0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
-        int [][] sudoku = {{0, 0, 3, 0, 2, 0, 6, 0, 0},
-                {9, 0, 0, 3, 0, 5, 0, 0, 1},
-                {0, 0, 1, 8, 0, 6, 4, 0, 0},
-                {0, 0, 8, 1, 0, 2, 9, 0, 0},
-                {7, 0, 0, 0, 0, 0, 0, 0, 8},
-                {0, 0, 6, 7, 0, 8, 2, 0, 0},
-                {0, 0, 2, 6, 0, 9, 5, 0, 0},
-                {8, 0, 0, 2, 0, 3, 0, 0, 9},
-                {0, 0, 5, 0, 1, 0, 3, 0, 0}};
+        System.out.println("El siguiente programa resuelve sudokus que reciba por el teclado");
+        System.out.println("Las casillas en blanco es escriben como un cero (0)");
+        System.out.println("----------------------------------------------------------------");
+
+        for (int i = 0; i < 9; i++){
+            switch (i) {
+                case 0:
+                    System.out.println("Por favor introduce los números de la primera fila:");
+                    break;
+                case 1:
+                    System.out.println("Por favor introduce los números de la segunda fila:");
+                    break;
+                case 2:
+                    System.out.println("Por favor introduce los números de la tercera fila:");
+                    break;
+                case 3:
+                    System.out.println("Por favor introduce los números de la cuarta fila:");
+                    break;
+                case 4:
+                    System.out.println("Por favor introduce los números de la quinta fila:");
+                    break;
+                case 5:
+                    System.out.println("Por favor introduce los números de la sexta fila:");
+                    break;
+                case 6:
+                    System.out.println("Por favor introduce los números de la séptima fila:");
+                    break;
+                case 7:
+                    System.out.println("Por favor introduce los números de la octava fila:");
+                    break;
+                case 8:
+                    System.out.println("Por favor introduce los números de la novena fila:");
+                    break;
+            }
+            for (int j = 0; j < 9; j++){ //Se llena toda la fila
+                System.out.print("Celda #" + (j+1) + ": ");
+                sudoku[i][j] = Sudoku.insertar(teclado);
+            }
+
+            do{
+                respuesta = teclado.next().charAt(0);
+                if (respuesta == 'n' && respuesta == 'N'){
+                    System.out.println("¿Cuál celda se debe modificar?");
+                    input = (teclado.nextInt() + 1);
+                    System.out.print("Celda #" + (input+1) + ": ");
+                    sudoku[i][input] = Sudoku.insertar(teclado);
+                    System.out.println("¿La fila actual es correcta? S/N");
+                    Matriz.printValores(sudoku);
+                }
+            } while (respuesta != 's' && respuesta != 'S'); //Sólo continúa a la siguiente fila cuando no hay errores
+
+            respuesta = 'x'; //Reiniciando valor de respuesta
+        }
 
         int N = sudoku.length;
         boolean[][] visitados = new boolean [N][N];
 
+        System.out.println("La solución del sudoku es la siguiente:");
         Sudoku.solucion(sudoku, visitados);
     }
 }
